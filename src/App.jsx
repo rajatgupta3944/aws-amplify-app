dc4d15b798b56639a9261dfdc3592e0fb11c7aa9
Rajat Gupta <rajatgupta@ideaclan.com>
2025-02-06 11:45:54 +0530

    authorization implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import AppRouterProvider from "./routes"; // ✅ Import Router Provider
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import awsExports from "./aws-exports";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(awsExports);
 
 function App() {
-  return <AppRouterProvider />;
+  return (
+    <Authenticator loginMechanism={["email"]}>
+      <AppRouterProvider />
+    </Authenticator>
+  );
 }
 
 export default App;
